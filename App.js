d6f7eaeccf99273fc1c83f8cfbf371f16dd3b474
keunji0708 <keunji0708@naver.com>
2022-09-19 00:22:38 +0900

    Core Component/Style RN

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import reactDom from 'react-dom';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to hahah </Text>
+      {/* inilne style */}
+      <Text style={{margin:16, borderWidth: 2, borderColor: 'red'}}>
+        Hello World!
+      </Text>
       <StatusBar style="auto" />
+      <View style={styles.container2}>
+        <Text>It's me!</Text>
+      </View>
+      <Button title='Tabs me!!'/>
     </View>
   );
 }
@@ -14,6 +22,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color : '#234343'
+  },
+  container2: {
+    backgroundColor: '#eabbbb',
     alignItems: 'center',
     justifyContent: 'center',
     color : '#234343'
