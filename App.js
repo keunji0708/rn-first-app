aaaaa7fbe3679f80d112c3de4b30993ab697719d
keunji0708 <keunji0708@naver.com>
2022-09-26 23:11:10 +0900

    flexbox justifyContent, alignItems 2

--- App.js
+++ App.js
@@ -14,19 +14,22 @@ export default function App() {
         <Text>It's me!</Text>
       </View>
       <Button title='Tabs me!!'/>
+      
       <View style={{
         padding : 50, 
         flexDirection : 'row', /*row는 left-to-right, column은 top-to-bottom*/
-        width : '80%', height : 300
+        width : '80%', height : 400, 
+        justifyContent:'center', /* align content : main axis top to bottom */
+        alignItems:'stretch' /* align content : cross axis left to right */
       }}>
         {/* Flexbox */}
-        <View style ={{backgroundColor:'red', justifyContent:'center', alignItems:'center'}}>
+        <View style ={{backgroundColor:'red', flex: 1, justifyContent:'flex-end', alignItems:'center'}}>
           <Text>Hahaha</Text>
         </View>
-        <View style ={{backgroundColor:'blue', justifyContent:'center', alignItems:'center'}}>
+        <View style ={{backgroundColor:'blue', flex: 2, justifyContent:'space-between', alignItems:'stretch'}}>
           <Text>Lalala</Text>
         </View>
-        <View style ={{backgroundColor:'green', justifyContent:'center', alignItems:'center'}}>
+        <View style ={{backgroundColor:'green', flex: 3, justifyContent:'center', alignItems:'flex-start'}}>
           <Text>Yayaya</Text>
         </View>
       </View>
